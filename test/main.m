9e10200141fb7bcfd49bb00c761009f5b9b416b5
Zer0f <shepherdalbert@ShepherdAlbertdeMacBook-Pro.local>
2015-07-15 17:39:56 +0800

    Initial Commit

---
+++ test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test
+//
+//  Created by ShepherdAlbert on 15/7/15.
+//  Copyright (c) 2015年 ShepherdAlbert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
